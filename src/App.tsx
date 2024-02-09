5c1fe7e6f6cdbdc1fa2650c93c29231676e4cdee
Felipe Bodelón <fjbodelon@uc.cl>
2024-02-09 12:20:22 -0300

    Update dependencies and add client.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { Button } from '@/components/ui/button';
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleCount() {
-    setCount(count + 1);
-  }
-
   return (
     <>
       <div>
-        <h1 className="p-4 pt-2 text-blue-700 ">React App</h1>
-        <Button onClick={handleCount}>Click me to add a value {count}</Button>
+        <h1 className="p-4 pt-2 text-blue-700 ">Vite React App</h1>
       </div>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   build: {
     sourcemap: true,
   },
